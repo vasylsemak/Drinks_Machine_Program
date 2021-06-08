e1093d9b710ee62c5d4bbce3ae4e9339ea7415de
Vasyl Semak <vasylsmk1401@gmail.com>
2021-06-07 23:42:32 -0400

    create components tree

---
+++ client/components/Coin.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Coin = () => (
+  <div className='coin'>
+    <p>Coin</p>
+  </div>
+)
+
+export default Coin
---
+++ client/components/Order.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Order = () => (
+  <div className='order'>
+    <h4>ORDER TOTAL: $ </h4>
+    <button>GET DRINKS</button>
+  </div>
+)
+
+export default Order
