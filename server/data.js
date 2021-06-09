e2fc92e9f4be1fb5fa9be78b124c43d3c09a77f8
Vasyl Semak <vasylsmk1401@gmail.com>
2021-06-08 20:03:26 -0400

    create dummy data object, filled allCoins component with data; create coin input, local state to manage input

---
+++ server/data.js
@@ -0,0 +1,43 @@
+export const coins = [
+  {
+    idValue: 1,
+    name: 'Penny',
+    quantity: 10,
+  },
+  {
+    idValue: 5,
+    name: 'Nickel',
+    quantity: 5,
+  },
+  {
+    idValue: 10,
+    name: 'Dime',
+    quantity: 10,
+  },
+  {
+    idValue: 25,
+    name: 'Quater',
+    quantity: 25,
+  },
+]
+
+export const products = [
+    {
+      id: 1,
+      name: 'Coke',
+      cost: 25,
+      quantityAvailable: 5,
+    },
+    {
+      id: 2,
+      name: 'Pepsi',
+      cost: 36,
+      quantityAvailable: 15
+    },
+    {
+      id: 3,
+      name: 'Soda',
+      cost: 45,
+      quantityAvailable: 3
+    }
+  ]
